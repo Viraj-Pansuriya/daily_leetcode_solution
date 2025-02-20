ecd84bc2e8e41a5e667b24400652d7a77442df25
viraj.pansuriya <viraj.pansuriya@verse.in>
2025-02-20 21:32:45 +0530

    20 feb

---
+++ 20_Feb_2025_1980.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <type_traits>
+#include <tuple>
+#include <cmath>
+#include <cstdlib>
+#include <complex>
+#include <valarray>
+#include <ctime>
+#include <cassert>
+#include <cctype>
+#include <cstdarg>
+#include <climits>
+#include <cfloat>
+#include <bitset>
+#define ll long long
+using namespace std;
+
+class Solution {
+    public:
+        string findDifferentBinaryString(vector<string>& nums) {
+         
+    
+    
+            string ans = ""; 
+           for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i][i] == '1')
+                ans += '0'; 
+            else ans += '1'; 
+           }
+    
+           return ans; 
+           
+    
+        }
+    };
