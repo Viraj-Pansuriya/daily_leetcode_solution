1f7a3cccf0789cd7a3ea2079d849733bb0d9d9f4
viraj.pansuriya <viraj.pansuriya@verse.in>
2025-02-21 09:36:25 +0530

    21 feb

---
+++ 21_Feb_2025_1261.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <type_traits>
+#include <tuple>
+#include <cmath>
+#include <cstdlib>
+#include <complex>
+#include <valarray>
+#include <ctime>
+#include <cassert>
+#include <cctype>
+#include <cstdarg>
+#include <climits>
+#include <cfloat>
+#include <bitset>
+#define ll long long
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+    public:
+        unordered_map < int , int  > st;   
+        FindElements(TreeNode* root) {
+            itr(root , 0); 
+        }
+    
+    
+        void itr( TreeNode * root , int val){
+            if(root == nullptr) return ; 
+            st[val]=1; 
+            itr(root->left , val*2 + 1); 
+            itr(root->right , val*2 + 2);  
+        }
+        
+        bool find(int target) {
+            return st.find(target) != st.end(); 
+        }
+    };
+    
+    /**
+     * Your FindElements object will be instantiated and called as such:
+     * FindElements* obj = new FindElements(root);
+     * bool param_1 = obj->find(target);
+     */
