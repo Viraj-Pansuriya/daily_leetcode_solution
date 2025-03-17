246cb1eb44b73dc5d2dd83c68fa745bd11dfd1da
viraj.pansuriya <viraj.pansuriya@verse.in>
2025-03-17 15:48:59 +0530

    17 mar

---
+++ 17_mar_2025_2206.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <type_traits>
+#include <tuple>
+#include <cmath>
+#include <cstdlib>
+#include <complex>
+#include <valarray>
+#include <ctime>
+#include <cassert>
+#include <cctype>
+#include <cstdarg>
+#include <climits>
+#include <cfloat>
+#include <bitset>
+#define ll long long
+using namespace std;
+
+class Solution {
+    public:
+        bool divideArray(vector<int>& nums) {
+            
+            vector < int > store(501 , 0); 
+            for(auto & x : nums) store[x]++;
+    
+            for(auto & x : store){
+                if(x & 1) return false; 
+            }
+            return true; 
+        }
+    };
